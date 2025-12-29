772b2985e4806bc74b19b763eb8f4f8d3222f2aa
ZhenfengSu <ZhenfengSu@outlook.com>
2025-12-29 09:16:45 +0000

    update run.bash

--- Entropy_Attention_Map/Attention_Map/run.bash
+++ Entropy_Attention_Map/Attention_Map/run.bash
@@ -1,4 +1,4 @@
 # https://zhuanlan.zhihu.com/p/1897638897388873200
 MODEL_PATH="/mnt/lc_share/modelscope/models/Qwen/WebAgent/WebSailor-3B"
 # 只分析 Baseline
-python block_attention.py --model_path $MODEL_PATH --input_json trajectories.json --output_dir ./output --sample_idx 0
+ python block_attention.py $MODEL_PATH
